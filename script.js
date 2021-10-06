9fcb3b465f7fb9cbb97186d21b0ab96dbf93f5aa
Shashi Gupta <shashi.gupta@payu.in>
2021-10-06 14:30:00 +0530

    added submit button

--- script.js
+++ script.js
@@ -1,71 +1,84 @@
-const chartData = {};
+
 window.addEventListener("load", function() {
-    const params = {}
-    let activeTab = 1;
-    //get chart data
-    function getChartData (intervalInDay, params) {
-        const url = `http://localhost:5000/mock/${intervalInDay}.json${Object.keys(params).length ? '?' : ''}${new URLSearchParams(params)}`;
-        // const url = `http://localhost:443/api/getAvgResponse/${intervalInDay}${Object.keys(params).length ? '?' : ''}${new URLSearchParams(params)}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            chartData[intervalInDay] = data;
-            intiateChartRender(intervalInDay);
-            getDRChartData(intervalInDay);
+    var clipRows=[];
+    function paste() {
+        var clipText;
+        var clipTable;
+        navigator.clipboard.readText()
+        .then(text => {
+            clipText = text;
+            console.log('Pasted content: ', text);
+            clipRows = clipText.split("\n");
+            for (i=0; i<clipRows.length; i++) {
+                // clipTable[];
+                clipRows[i] = clipRows[i].split(String.fromCharCode(9));
+            }
+            // write out in a table
+            newTable = document.createElement("table")
+            newTable.border = 1;
+            for (i=0; i<clipRows.length; i++) {
+                newRow = newTable.insertRow();
+                for (j=0; j<clipRows[i].length; j++) {
+                    newCell = newRow.insertCell();
+                    if (clipRows[i][j].length == 0) {
+                        newCell.innerText = ' ';
+                    }
+                    else {
+                        if(i === 0 && clipRows[i][j].includes(":0")) {
+                            newCell.style.backgroundColor = "white";
+                        }
+                        newCell.innerText = clipRows[i][j];
+                    }
+                }
+            }
+            document.getElementById("content").appendChild(newTable);
+            document.getElementById("submit").style.display = "block";
+        })
+        .catch(err => {
+            console.error('Failed to read clipboard contents: ', err);
         });
     }
-    //fetch default chart data on load
-    getChartData(activeTab, params);
-    
-	// store tabs variable
-	var myTabs = document.querySelectorAll("div.nav-tabs > .nav-item");
-  function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-        var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-            myContentPanes[i].classList.remove("active");
-		}
-        var anchorReference = tabClickEvent.target;
-        activeTab =  anchorReference.getAttribute('data-day');
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
-        activePane.classList.add("active");
-        getChartData(activeTab, params);
-	}
-	for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-    }
-    var stateOption = document.getElementById('stateOption');
-    var productType = document.getElementById('productType');
-    var tenantId = document.getElementById('tenantId');
-    var submitBtn = document.getElementById('submitBtn');
-    function onStateChange() {
-        updateParamsValue('State', this.value)
-    }
-    function onProductTypeChange() {
-        updateParamsValue('ProductType', this.value)
-    }
-    function onTenantIdChange() {
-        updateParamsValue('TenantId', this.value)
-    }
-    function updateParamsValue(key, value) {
-        if(value === "-1") {
-            delete params[key]
-        } else {
-            params[key]= value;
+    function copy() {
+        var table = document.getElementsByTagName('table');
+        navigator.clipboard.writeText(table[0].innerText);
+    } 
+    async function submit() {
+        const result = [];
+        console.log(clipRows);
+        for(i = 0; i<clipRows.length - 1; i++) {
+            var row = {}
+            for(j = 0; j < clipRows[i].length; j++) {
+                row[clipRows[0][j]] = clipRows[i+1][j] && clipRows[i+1][j].replace(/\r?\n|\r/, "");
+            }
+            result.push(row);
         }
+        postData('https://google.com', {data: result});
     }
 
-    stateOption.addEventListener("change", onStateChange);
-    productType.addEventListener("change", onProductTypeChange);
-    tenantId.addEventListener("change", onTenantIdChange);
-    submitBtn.addEventListener("click", () => {
-        getChartData(activeTab, params);
+    async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+    document.getElementById("copy").addEventListener("click", function() {
+        copy();
     });
-    
+    document.getElementById("paste").addEventListener("click", function() {
+        paste();
+    });
+    document.getElementById("submit").addEventListener("click", function() {
+        submit();
+    });
 });
